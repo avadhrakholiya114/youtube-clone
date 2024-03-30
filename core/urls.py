60fa6b5cd483c6ad5f40243be981e1a650c0ee24
avadh <rakholiyabalo@gmail.com>
2024-03-30 11:07:28 +0530

    subsccribe unsbscribe funationality

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("video/<int:id>", views.detail, name="video"),
 
+    # comment
     path('save_comment/', views.save_comment, name='save_comment'),
     path('delete_comment/', views.delete_comment, name='delete_comment'),
+
+    # subscriber
+
+    path('new_subsciber/<int:id>/', views.new_subscriber, name='new_subsciber'),
+    path('load_sub/<int:id>/', views.load_sub, name='load_sub')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,HttpResponse
-from .models import Video,Comment
-from django.http import  JsonResponse
+from django.shortcuts import render, HttpResponse
+from .models import Video, Comment
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from channel.models import *
 
 
 # Create your views here.
@@ -9,48 +10,82 @@ from django.views.decorators.csrf import csrf_exempt
 def index(request):
     video = Video.objects.filter(visibility='public');
 
-    context={
-        "video" : video
+    context = {
+        "video": video
     }
-    return render(request,'index.html',context)
+    return render(request, 'index.html', context)
 
-def detail(request,id):
-    video=Video.objects.get(id=id)
+
+def detail(request, id):
+    video = Video.objects.get(id=id)
 
     # when ever this url hit add 1 view
-    video.views= video.views+1
+    video.views = video.views + 1
     video.save()
 
-    #getting all commment releted to video
+    # getting all commment releted to video
 
-    comment=Comment.objects.filter(video=video,active=True).order_by("-date")
+    comment = Comment.objects.filter(video=video, active=True).order_by("-date")
     comment_count = comment.count()
-
+    channel = Channel.objects.get(id=video.user.id)
     context = {
         "video": video,
-        "comment":comment,
+        "comment": comment,
         "comment_count": comment_count,
+        "channel": channel
     }
     return render(request, 'video.html', context)
 
+
 def save_comment(request):
     if request.method == "POST":
-        pk=request.POST.get("id")
+        pk = request.POST.get("id")
         comment = request.POST.get("comment")
-        video=Video.objects.get(id=pk)
+        video = Video.objects.get(id=pk)
         user = request.user
         comment = Comment.objects.create(comment=comment, user=user, video=video)
         comment.save()
 
-        response="done"
-        return  HttpResponse(response)
+        response = "done"
+        return HttpResponse(response)
+
+    # Cross
+    # Site
+    # Request
+    # Forgery(CSRF)
+    # delete_comment view is marked with @ csrf_exempt.This means that CSRF protection will not be applied to this view.
+
 
 @csrf_exempt
 def delete_comment(request):
-    if request.method =='POST' :
+    if request.method == 'POST':
         id = request.POST.get("cid")
         comment = Comment.objects.get(id=id)
         comment.delete()
         return JsonResponse({"status": 1})
     else:
         return JsonResponse({"status": 2})
+
+
+from django.http import JsonResponse
+
+def new_subscriber(request, id):
+    subscriber = Channel.objects.get(id=id)
+    user = request.user
+
+    if user in subscriber.subscriber.all():
+        # remove because user is already there
+        subscriber.subscriber.remove(user)
+        response = "subscribe"
+    else:
+        subscriber.subscriber.add(user)
+        response = "unsubscribe"
+
+    return JsonResponse({'response': response})
+
+
+def load_sub(request, id):
+    subscriber = Channel.objects.get(id=id)
+    sub_list = list(subscriber.subscriber.values())
+    return JsonResponse({'subscribers': sub_list})
+
